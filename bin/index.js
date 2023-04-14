3290c6fffe127684ba56bdbd6d00e6dab815f0eb
cyber-storm-200712 <leonsummer2007@gmail.com>
2023-04-15 05:47:50 +0900

    command senser added

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,15 @@
 #! /usr/bin/env node 
 'use strict'
 
-const yargs = require("yargs")
+const commandLineArgs = require('command-line-args');
+const optionDefinitions = [
+    { name: 'layout', alias: 'l', type: String},
+    { name: 'traits', type: String, multiple: true, defaultOption: true, alias: 't' }
+]
+
 
 console.log("NFT generation is upcoming.");
 
 console.log(process.cwd())
+
+console.log(commandLineArgs(optionDefinitions))
